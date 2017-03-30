d7e72b4b3dfba7e59bc248203696acdaf63776be
Seow Yung Hoe <seowyunghoe@hotmail.com>
2017-03-30 13:11:55 +0800

    first commit

---
+++ MobileTest(SeowYungHoe)/Doctor.swift
@@ -0,0 +1,9 @@
+//
+//  Doctor.swift
+//  MobileTest(SeowYungHoe)
+//
+//  Created by Seow Yung Hoe on 29/03/2017.
+//  Copyright © 2017 Seow Yung Hoe. All rights reserved.
+//
+
+import Foundation
---
+++ MobileTest(SeowYungHoe)/DoctorCell.swift
@@ -0,0 +1,24 @@
+//
+//  DoctorCell.swift
+//  MobileTest(SeowYungHoe)
+//
+//  Created by Seow Yung Hoe on 29/03/2017.
+//  Copyright © 2017 Seow Yung Hoe. All rights reserved.
+//
+
+import UIKit
+
+class DoctorCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
